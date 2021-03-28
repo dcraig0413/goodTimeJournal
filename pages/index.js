7f6182d79268571060aaf0a7689bfeb210870be6
David Craig <david@methoddata.com>
2021-03-27 21:13:39 -0400

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,41 @@
 //[label /pages/index.js]
-import Link from 'next/link';
-import React, { Component } from 'react'
+import Link from "next/link";
+import React, { Component } from "react";
+import makeStyles from "@material-ui/styles";
 
 const linkStyle = {
-  marginRight: 15
-}
+  marginRight: 15,
+};
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "blue",
+    margin: "auto auto",
+  },
+});
+const classes = useStyles();
 
 export default class extends Component {
-  render () {
+  render() {
     return (
       <div>
-      <div>Good Time Journal HomeScreen</div>
-      <div> 
-      <div>
-          <Link href="/SignUp">
-            <button>Sign Up</button>
-          </Link>
-          
-          <Link href="/Delete">
-            <button>Delete</button>
-          </Link>
+        <div className={classes.root}>Good Time Journal HomeScreen</div>
+        <div>
+          <div>
+            <Link href="/SignUp">
+              <button>Sign Up</button>
+            </Link>
 
-          <Link href="/Login">
-            <button>Login</button>
-          </Link>
+            <Link href="/Delete">
+              <button>Delete</button>
+            </Link>
+
+            <Link href="/Login">
+              <button>Login</button>
+            </Link>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-    )
+    );
   }
 }
-
-
